7f3ad1a3a1f8a0242ae68f389b584d4b7dfd1b9e
Erik <epautsch@luc.edu>
2024-06-28 02:05:43 -0500

    updated fedavg.sh for aisec1 runs

--- script/fedavg.sh
+++ script/fedavg.sh
@@ -1,2 +1,2 @@
 cd .. &&
-python FedAverage.py --data='imdb' --nclient=100 --nclass=2 --ncpc=2 --model='SentimentClassifier' --mode='LDP' --round=60 --epsilon=2 --sr=1 --lr=5e-3 --flr=1e-2 --physical_bs=64 --E=1
+python FedAverage.py --data='imdb' --nclient=3 --nclass=2 --ncpc=2 --model='SentimentClassifier' --mode='LDP' --round=60 --epsilon=2 --sr=1 --lr=5e-3 --flr=1e-2 --physical_bs=8192 --E=1
